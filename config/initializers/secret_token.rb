95fa5e951b31224d130923ba228af7fe45c5ceba
Andy Hayes <andythemanhayes@hotmail.com>
2014-03-01 10:13:15 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datatracker::Application.config.secret_token = '4670de1296d3d19e7f5403e0d20d46115b821fe4640421a0288238ce4ee52be73762bfb355e73a2cb4f7f274fcc9f499d06009f9425063169a2f6516745aacbd'
