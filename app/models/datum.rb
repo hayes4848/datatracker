80d4b7cc6f3cf07c11234a8fd672c1c35f831af7
ViCtor Jr <NewUser@ViCtor-Jr.local>
2014-03-01 15:50:57 -0700

    changes fixing chart and adding data model

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,3 +1,56 @@
 class Datum < ActiveRecord::Base
+
+
+
   attr_accessible :boron_cracks, :holes, :light_leaks, :specks
+
+
+  def self.specks_data
+    [monday('specks'), tuesday('specks'), wednesday('specks'), thursday('specks'), friday('specks'), last_week('specks')].join(', ')
+  end
+
+
+  def self.light_leaks_data
+    [monday('specks'), tuesday('specks'), wednesday('specks'), thursday('specks'), friday('specks'), last_week('specks')].join(', ')
+  end
+
+
+  def self.holes_data
+    [monday('specks'), tuesday('specks'), wednesday('specks'), thursday('specks'), friday('specks'), last_week('specks')].join(', ')
+  end
+
+
+  def self.boron_cracks_data
+    [monday('specks'), tuesday('specks'), wednesday('specks'), thursday('specks'), friday('specks'), last_week('specks')].join(', ')
+  end
+
+
+  private
+  def self.monday(day)
+
+  end
+
+  def self.tuesday(day)
+
+  end
+
+  def self.wednesday(day)
+
+  end
+
+  def self.thursday(day)
+
+  end
+
+  def self.friday(day)
+
+  end
+
+
+  def self.last_week(day)
+
+  end
+
+
+
 end
